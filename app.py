9ce6cfa93fa96ca027c675d6adb72db34b0f9807
Nicodemus Opon <nicodemusopon@gmail.com>
2019-01-09 10:50:55 +0300

    hhk

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# app.debug(True)
+
+@app.route('/', methods=["GET", "POSTT"])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/registration', methods=["GET", "POSTT"])
+def reg():
+    return render_template('registration.html')
+
+
+@app.route('/fees', methods=["GET", "POSTT"])
+def fees():
+    return render_template('blank.html')
+
+
+@app.route('/exams', methods=["GET", "POSTT"])
+def exams():
+    return render_template('exams.html')
+
+
+@app.route('/inventory', methods=["GET", "POSTT"])
+def inventory():
+    return render_template('inventory.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
